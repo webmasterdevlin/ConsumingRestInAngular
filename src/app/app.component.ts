cf12fda8fffb5c2e3165e121923747e6819b86b5
Devlin Duldulao <webmasterdevlin@gmail.com>
2018-02-09 10:30:21 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CoffeeService} from './services/coffee.service';
+import {ICoffee} from './models/coffee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  coffees: ICoffee[] = [];
+  constructor(private _coffeeServices: CoffeeService) {}
+  showTypesOfCoffees() {
+    return this._coffeeServices.getCoffees().subscribe(data => {
+      console.log(data);
+      this.coffees = data;
+    }, error => console.log(error) );
+  }
+  ngOnInit() {
+    return this.showTypesOfCoffees();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {CoffeeService} from './services/coffee.service';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -10,9 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [CoffeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
